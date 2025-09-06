f7b92b9dec0874713e76b27ef42f6f69a3375b1f
SDaKko <danilkosenko172004@mail.ru>
2025-09-07 00:51:39 +0300

    Лаб1. Задания 1-3.
    Установлена библиотека OpenCV. Изображения и видео выведены на экран. Использованы различные флаги для изображений и разные форматы отображения видео, в частности размеры и цветовая гамма.

---
+++ test_images_videos.py
@@ -0,0 +1,50 @@
+# Задание 1
+import cv2
+
+# Задание 2
+img = cv2.imread(r'C:\Pics\photo.png', cv2.IMREAD_GRAYSCALE)
+img2 = cv2.imread(r'C:\Pics\zakat.jpg', cv2.IMREAD_COLOR)
+img3 = cv2.imread(r'C:\Pics\waterfall.gif', cv2.IMREAD_COLOR_RGB)
+cv2.namedWindow('output', cv2.WINDOW_NORMAL)
+cv2.imshow('output', img)
+cv2.waitKey(1000)
+cv2.namedWindow('output2', cv2.WINDOW_AUTOSIZE)
+cv2.imshow('output2', img2)
+cv2.waitKey(1000)
+cv2.namedWindow('output3', cv2.WINDOW_FULLSCREEN)
+cv2.imshow('output3', img3)
+cv2.waitKey(1000)
+
+# Задание 3
+cap = cv2.VideoCapture(r'C:\Videos\video.mp4', cv2.CAP_ANY)
+
+fps = cap.get(cv2.CAP_PROP_FPS)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+print(f"Размер: {width}x{height}")
+print(f"FPS: {fps}")
+print(f"Всего кадров: {frame_count}")
+
+while True:
+    ret, frame = cap.read()
+
+    if not(ret):
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    resized_down = cv2.resize(frame, (width // 2, height // 2))
+    # cv2.imshow('Размеры уменьшены в 2 раза', resized_down)
+
+    # cv2.imshow('frame', hsv)
+    # cv2.imshow('frame', gray)
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
