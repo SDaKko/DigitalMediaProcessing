48bdd650d573211857350a3431614b4d7d9dff15
SDaKko <danilkosenko172004@mail.ru>
2025-09-07 15:38:01 +0300

    Лаб1. Задание 6
    Прочитано изображение с камеры, выведен красный крест в центре.

--- test_images_videos.py
+++ test_images_videos.py
@@ -1,28 +1,6 @@
 # Задание 1
 import cv2
 
-def read_write_file(input_video_path, output_video_path):
-    cap = cv2.VideoCapture(input_video_path)
-    w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    # fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    video_writer = cv2.VideoWriter(output_video_path, fourcc, fps, (w, h))
-    while (True):
-        ret, frame = cap.read()
-
-        if not (ret):
-            break
-
-        cv2.imshow('video2', frame)
-        video_writer.write(frame)
-        if cv2.waitKey(1) & 0xFF == 27:
-            break
-    cap.release()
-    cv2.destroyAllWindows()
-
-
 # Задание 2
 # img = cv2.imread(r'C:\Pics\photo.png', cv2.IMREAD_GRAYSCALE)
 # img2 = cv2.imread(r'C:\Pics\zakat.jpg', cv2.IMREAD_COLOR)
@@ -72,6 +50,27 @@ cap.release()
 cv2.destroyAllWindows()
 
 # Задание 4
+def read_write_file(input_video_path, output_video_path):
+    cap = cv2.VideoCapture(input_video_path)
+    w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    # fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    video_writer = cv2.VideoWriter(output_video_path, fourcc, fps, (w, h))
+    while (True):
+        ret, frame = cap.read()
+
+        if not (ret):
+            break
+
+        cv2.imshow('video2', frame)
+        video_writer.write(frame)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
 input_path = r'C:\Videos\video.mp4'
 output_path = r'C:\Videos\output.mp4'
 # output_path = r'C:\Videos\output.avi'
@@ -87,5 +86,56 @@ cv2.imshow("hsv_img_new", hsv_img_new)
 cv2.waitKey(1000)
 cv2.destroyAllWindows()
 
+# Задание 6
+def print_cam():
+    cap = cv2.VideoCapture(0)
+
+    cap.set(3, 640)
+    cap.set(4, 480)
+    w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    center_x, center_y = w // 2, h // 2
+    cross_size = 90
+    thickness = 25
+
+    while True:
+        ret, frame = cap.read()
+
+        if not (ret):
+            break
+
+        h_start_y = center_y - thickness // 2
+        h_end_y = center_y + thickness // 2
+        h_start_x = center_x - cross_size
+        h_end_x = center_x + cross_size
+        horizontal_region = frame[h_start_y:h_end_y, h_start_x:h_end_x]
+        blurred_horizontal = cv2.blur(horizontal_region, (25, 25))
+        frame[h_start_y:h_end_y, h_start_x:h_end_x] = blurred_horizontal
+
+        # Горизонтальная полоса
+        cv2.rectangle(frame,
+                      (center_x - cross_size, center_y - thickness // 2),
+                      (center_x + cross_size, center_y + thickness // 2),
+                      (0, 0, 255), 0)
+
+        # Вертикальная полоса
+        cv2.rectangle(frame,
+                      (center_x - thickness // 2, center_y - cross_size),
+                      (center_x + thickness // 2, center_y - thickness // 2),
+                      (0, 0, 255), 0)
+        cv2.rectangle(frame,
+                      (center_x - thickness // 2, center_y + thickness // 2),
+                      (center_x + thickness // 2, center_y + cross_size),
+                      (0, 0, 255), 0)
+
+        cv2.imshow("camera", frame)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+print_cam()
 
 
